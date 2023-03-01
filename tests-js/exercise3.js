f181ce621f2628840132e2ec3729f11802d9b920
Santi <santiloureiro12@gmail.com>
2023-03-01 18:42:17 -0300

    finished hmtl test

--- tests-js/exercise3.js
+++ tests-js/exercise3.js
@@ -5,26 +5,23 @@ let number2 = 50
 
 
 const primeNumbersOnRange = (num1, num2) => {
-    
-    let numbers = []
 
     let primes = []
 
     let indicator = false
 
     for (let i = 0; i < num2 - 1; i++) {
-        numbers.push(num1)
         for (let j = 2; j <= num1 - 1; j++) {
            if(num1 % j == 0){
                 indicator = true
            }
         }
+        
         if(!indicator) primes.push(num1)
         indicator = false
         num1++
     }
 
-    console.log(`Range of numbers ${numbers}`)
     console.log(`Prime Numbers on the range: ${primes}`)
 }
 
