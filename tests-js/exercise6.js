0826fbe97a871460acbc4d96278a3cd7c30bf3c9
Santi <santiloureiro12@gmail.com>
2023-03-02 14:06:45 -0300

    corrected ex 6

--- tests-js/exercise6.js
+++ tests-js/exercise6.js
@@ -34,7 +34,6 @@ const cars = [
     },
 ]
 
-
 const getArrayWithProp = (array, prop, value) => {
     const arrayWithProp = array.filter(el => el[prop].toLowerCase()  == value.toLowerCase());
     return arrayWithProp
@@ -44,3 +43,80 @@ console.log(getArrayWithProp(cars ,"color", "blue"))
 console.log(getArrayWithProp(cars ,"size","Medium"))
 console.log(getArrayWithProp(cars ,"color","Red"))
 console.log(getArrayWithProp(cars ,"size","Small"))
+
+
+// Originalmente interprete el ejercicio 6 como un filtro al objeto principal,
+// aqui quedaria el ejercicio rehecho
+
+const carsByColor = [{
+    blue: [    {
+        color: "Blue",
+        size: "Large"
+    },
+    {
+        color: "Blue",
+        size: "Large"
+    },
+    {
+        color: "Blue",
+        size: "Medium"
+    },
+    {
+        color: "Blue",
+        size: "Large"
+    }
+],
+    red: [{
+        color: "Red",
+        size: "Small"
+    },
+    {
+        color: "Red",
+        size: "Large"
+    },
+    {
+        color: "Red",
+        size: "Small"
+    },
+    {
+        color: "Red",
+        size: "Medium"
+    }
+]
+}]
+
+const carsBySize = [{
+    small: [    {
+        color: "Red",
+        size: "Small"
+    },
+    {
+        color: "Red",
+        size: "Small"
+    }],
+    medium: [    {
+        color: "Blue",
+        size: "Medium"
+    },
+    {
+        color: "Red",
+        size: "Medium"
+    }],
+    large: [ {
+        color: "Blue",
+        size: "Large"
+    },
+    {
+        color: "Blue",
+        size: "Large"
+    },
+    {
+        color: "Red",
+        size: "Large"
+    },
+    {
+        color: "Blue",
+        size: "Large"
+    }]
+
+}]
